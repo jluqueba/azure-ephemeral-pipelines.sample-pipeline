f543c62d095fdd8c337a7a4c31ee249344e8cc13
x284783 <x284783@es.produban.com>
2020-03-28 11:08:13 +0100

    Remove unsoported resource name

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "azurerm" {
 
 resource "azurerm_storage_blob" "blob" {
   name                   = "testfile.txt"
-  resource_group_name    = var.resource_group_name
   storage_account_name   = var.storage_account_name
   storage_container_name = var.storage_container_name
   type                   = "Block"
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "resource_group_name" {
-  type = string
-  description = "(Required) Specifies the name of the Resource Group in which exists the target storage account to upload the blob"
-}
-
 variable "storage_account_name" {
   type = string
   description = "(Required) Specifies the name of the Storage Account in which upload the blob"
